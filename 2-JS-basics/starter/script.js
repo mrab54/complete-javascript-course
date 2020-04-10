b084322481528b1618bd6552fb47be09d2c36f3e
MR <mrab54@gmail.com>
2020-04-10 16:42:10 -0500

    Work

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,15 @@
+var jScore1 = 89;
+var jScore2 = 120;
+var jScore3 = 103;
+
+var mScore1 = 116;
+var mScore2 = 94;
+var mScore3 = 123;
+
+var jAvg = (jScore1 + jScore2 + jScore3) / 3;
+var mAvg = (mScore1 + mScore2 + mScore3) / 3;
+
+console.log('mAvg = ' + mAvg);
+console.log('jAvg = ' + jAvg);
+mAvg > jAvg ? console.log('Mike ') : console.log('John ');
+console.log('team had a higher average score');
